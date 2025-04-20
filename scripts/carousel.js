7f0edfc97b418f903d2195f1aabf3e9fcf833816
HarmonyH27 <159847908+HarmonyH27@users.noreply.github.com>
2025-04-19 21:40:09 -0400

    Update Student Project
    - Add the Swiper Carousel
    - Add images to the Carousel
    - Left space for two projects and removed the other projects

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,4 +1,3 @@
-
 var swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
@@ -6,46 +5,3 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
- 
-//  var swiper = new Swiper(".bulletinboard", {
-//     navigation: {
-//         nextEl: ".carousel-button-next",
-//         prevEl: ".carousel-button-prev",
-//     },
-//  });
-
-/* const accessCarousel = () => {
-    let carousel = document.getElementsByClassName("carousel");
-    let carousel_items = document.getElementsByClassName("image-stack");
-    let prev_button = document.getElementById("previous");
-    let next_button = document.getElementById("next");
-    console.log(carousel);
-    console.log(carousel_items);
-
-    let currentCarouselIndex = 0;
-
-    if (!carousel){
-        console.error("Cannot find carousel element");
-        return null;
-    }
-
-    if(!carousel_items){
-         console.error("Cannot find carousel item");
-        return null;
-    }
-}
-
-const moveSlide = (direction) =>{
-    
-    const newCarouselIndex = 0;
-
-    if(direction == "next"){
-        newCarouselIndex = (currentCarouselIndex + 1) % carousel.length;
-    }else{
-        newCarouselIndex = (currentCarouselIndex - 1 + carousel.length) % carousel.length;
-    }
-    currentCarouselIndex = newCarouselIndex;
-}
-
-accessCarousel()
-*/
